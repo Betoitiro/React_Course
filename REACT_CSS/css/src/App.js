0a57439ab5f899bed78d577ec2ddae1c4971b8a3
Betoitiro <itirotrabalhos@gmail.com>
2023-12-05 21:27:36 -0300

    react com css

--- REACT_CSS/css/src/App.js
+++ REACT_CSS/css/src/App.js
@@ -1,8 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import MyComponents from './components/MyComponents';
 
+import {useState} from 'react';
+
 function App() {
+  const n = 15;
+  const [name] = useState("humberto")
+
+  const redTitle = true;
   return (
     <div className="App">
       {/**css global */}
@@ -24,6 +29,19 @@ function App() {
       <p style={{color: "blue", padding: "25px"}}>
         Esse paragrafo é dedicado ao inline CSS
       </p>
+
+      {/**CSS inline dinamic */}
+      {/**
+       * o if ternario corresponde a criação de um atributo, e a seguinte estrutura:
+       *  n>10 ? ({acao caso o n seja maior que 10}) :({uma acao caso o n seja menor que 10})
+       */}
+      <h1 style={n >10 ? ({color: "purple"}):({color: "pink"})}>
+        CSS DINAMICO
+      </h1>
+      <h1 style={name==="humberto" ? ({color: "gren"}):({color: "black"})}>CSS DINAMICO</h1>
+
+       {/**CLasse dinamic */}
+       <h2 className={redTitle ? "red-title" : "title"}> Class dinamic</h2>
     </div>
   );
 }
