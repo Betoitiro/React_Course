9e73efd9fb682df3ba648f83ef269b11c711d508
Betoitiro <itirotrabalhos@gmail.com>
2023-12-05 22:57:29 -0300

    challengecss

---
+++ REACT_CSS/challengecss/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+
+  const mycars = [
+    {name: "fusca", km: 1000000, color:"Branca"},
+    {name: "Polo", km:320000, color: "Preto"},
+    {name: "Nivus", km:0, color:"Preto"}
+  ]
+
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
